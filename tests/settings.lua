84da5b00df970c20f01c7ac9348ee87743f7d541
fooofei <aihujianfei@gmail.com>
2017-09-24 09:03:07 +0800

    * lua

--- tests/settings.lua
+++ tests/settings.lua
@@ -2,6 +2,17 @@
 -- is better than txt config files
 a = "common_value"
 
+-- this can get path in execute by lua <script>,
+-- but cannot get path in embeded c as file
+function script_path()
+   local str = debug.getinfo(2, "S").source:sub(2)
+   return str:match("(.*/)")
+end
+
+function function_return()
+  return "string_returned_from_function"
+end
+
 settings = {
   resolution = {
     width = 800,
@@ -20,6 +31,8 @@ settings = {
       config_string2="config_string2_value",
       config_string_common1=a.."_append1", --append string
       config_string_common2=a.."_append2",
+      config_string_format=string.format("from_string_format"),
+      config_string_returned=function_return(),
     },
     config_bool={
       config_bool1=false,
