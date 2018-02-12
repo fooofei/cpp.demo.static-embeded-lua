f05f1c193445b85acb210cbbf9a7d2b604c27060
fooofei <aihujianfei@qq.com>
2018-02-12 09:22:34 +0800

    6

---
+++ lua_dirty/encode_string.h
@@ -0,0 +1,15 @@
+#ifndef ENCODE_STRING_H_
+#define ENCODE_STRING_H_
+
+
+
+/* the path encoding on Windows is ansi(mbcs)
+  convert it to utf8
+  if success, then return 1 same as other str* functions, like str_upper().
+  if fail, return 0
+*/
+struct lua_State;
+int 
+str_mbcs2utf8(struct lua_State *L);
+
+#endif //ENCODE_STRING_H_
